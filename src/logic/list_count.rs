f47bc2ccdee044767ae3a402438ebca2d237233f
silent-rain <2367221387@qq.com>
2025-04-25 11:16:50 +0800

    add List Count node

---
+++ src/logic/list_count.rs
@@ -0,0 +1,92 @@
+//! 列表计数
+
+use pyo3::{
+    pyclass, pymethods,
+    types::{PyAnyMethods, PyDict, PyType},
+    Bound, Py, PyAny, PyResult, Python,
+};
+
+use crate::core::{
+    types::{any_type, NODE_INT},
+    PromptServer,
+};
+
+/// 列表计算
+#[pyclass(subclass)]
+pub struct ListCount {}
+
+impl PromptServer for ListCount {}
+
+#[pymethods]
+impl ListCount {
+    #[new]
+    fn new() -> Self {
+        let _ = tracing_subscriber::fmt()
+            .with_max_level(tracing::Level::DEBUG)
+            .with_level(true)
+            .with_file(true)
+            .with_line_number(true)
+            .try_init();
+        Self {}
+    }
+
+    #[classmethod]
+    #[pyo3(name = "INPUT_TYPES")]
+    fn input_types(_cls: &Bound<'_, PyType>) -> PyResult<Py<PyDict>> {
+        Python::with_gil(|py| {
+            let dict = PyDict::new(py);
+            dict.set_item("required", {
+                let required = PyDict::new(py);
+                required.set_item(
+                    "any",
+                    (any_type(py)?, {
+                        let any = PyDict::new(py);
+                        any.set_item("tooltip", "Input any list")?;
+                        any
+                    }),
+                )?;
+                required
+            })?;
+            Ok(dict.into())
+        })
+    }
+
+    #[classattr]
+    #[pyo3(name = "INPUT_IS_LIST")]
+    fn input_is_list() -> bool {
+        true
+    }
+
+    #[classattr]
+    #[pyo3(name = "RETURN_TYPES")]
+    fn return_types() -> (&'static str,) {
+        (NODE_INT,)
+    }
+
+    #[classattr]
+    #[pyo3(name = "RETURN_NAMES")]
+    fn return_names() -> (&'static str,) {
+        ("int",)
+    }
+
+    #[classattr]
+    #[pyo3(name = "OUTPUT_IS_LIST")]
+    fn output_is_list() -> (bool,) {
+        (false,)
+    }
+
+    #[classattr]
+    #[pyo3(name = "FUNCTION")]
+    const FUNCTION: &'static str = "execute";
+
+    #[classattr]
+    #[pyo3(name = "CATEGORY")]
+    const CATEGORY: &'static str = "SilentRain/Logic";
+
+    #[pyo3(name = "execute")]
+    fn execute(&mut self, any: Vec<Bound<'_, PyAny>>) -> PyResult<(usize,)> {
+        let total = any.len();
+
+        Ok((total,))
+    }
+}
--- src/logic/mod.rs
+++ src/logic/mod.rs
@@ -1,17 +1,19 @@
 //! 逻辑
 
-pub mod index_any;
+pub mod index_anything;
+pub mod list_count;
 
 use pyo3::{
     types::{PyModule, PyModuleMethods},
     Bound, PyResult, Python,
 };
 
-pub use index_any::IndexAny;
+pub use index_anything::IndexAnything;
+pub use list_count::ListCount;
 
 /// 逻辑模块
 pub fn submodule(py: Python<'_>) -> PyResult<Bound<'_, PyModule>> {
     let submodule = PyModule::new(py, "logic")?;
-    submodule.add_class::<IndexAny>()?;
+    submodule.add_class::<IndexAnything>()?;
     Ok(submodule)
 }
