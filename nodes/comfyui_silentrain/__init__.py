fa82227cb80537e1eb91781e4dfe3bebf8abdc26
silent-rain <2367221387@qq.com>
2025-04-29 00:45:18 +0800

    build

--- nodes/comfyui_silentrain/__init__.py
+++ nodes/comfyui_silentrain/__init__.py
@@ -1,5 +1,3 @@
-from .comfyui_silentrain import *
-
-__doc__ = comfyui_silentrain.__doc__
-if hasattr(comfyui_silentrain, "__all__"):
-    __all__ = comfyui_silentrain.__all__
+version https://git-lfs.github.com/spec/v1
+oid sha256:6f876abff82414b641bf2ed3f447f4105682305d17156761eaa0555c34b17659
+size 155
