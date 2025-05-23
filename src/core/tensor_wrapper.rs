b204596b64a418dd8b0eefe1ad1cf9d8d2215099
silent-rain <2367221387@qq.com>
2025-05-23 18:10:10 +0800

    删除无效单测

--- src/core/tensor_wrapper.rs
+++ src/core/tensor_wrapper.rs
@@ -143,19 +143,6 @@ mod tests {
 
     use super::*;
 
-    // #[test]
-    // fn test_tensor() -> anyhow::Result<()> {
-    //     let data: [u32; 3] = [1u32, 2, 3];
-    //     let tensor = TensorWrapper::new(&data, &Device::Cpu)?;
-    //     println!("tensor: {:?}", tensor.to_vec1::<u32>()?);
-
-    //     let nested_data: [[u32; 3]; 3] = [[1u32, 2, 3], [4, 5, 6], [7, 8, 9]];
-    //     let nested_tensor = TensorWrapper::new(&nested_data, &Device::Cpu)?;
-    //     println!("nested_tensor: {:?}", nested_tensor.to_vec2::<u32>()?);
-
-    //     Ok(())
-    // }
-
     #[test]
     fn test_new() -> anyhow::Result<()> {
         Python::with_gil(|py| {
