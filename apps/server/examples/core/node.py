922286bca7b2797a532e2d1cc6446099c7083c7e
silent-rain <2367221387@qq.com>
2025-09-06 23:13:11 +0800

    添加python调用示例

--- apps/server/examples/core/node.py
+++ apps/server/examples/core/node.py
@@ -8,6 +8,7 @@ from comfyui_silentrain import sum_as_string
 # ComfyUI 注册节点导入
 from comfyui_silentrain import NODE_CLASS_MAPPINGS
 from comfyui_silentrain import NODE_DISPLAY_NAME_MAPPINGS
+from comfyui_silentrain import WEB_DIRECTORY
 
 
 print("comfyui_silentrain:")
@@ -21,4 +22,7 @@ print(sum_as_string(1, 2))
 print("\n\n")
 print("NODE_DISPLAY_NAME_MAPPINGS:", NODE_DISPLAY_NAME_MAPPINGS)
 print("NODE_CLASS_MAPPINGS:", NODE_CLASS_MAPPINGS)
+
+print("\n\n")
+print("WEB_DIRECTORY:", WEB_DIRECTORY)
 
