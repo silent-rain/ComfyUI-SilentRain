c929e840670bf6a64745e89f4bcb9e55b3d57ea4
silent-rain <2367221387@qq.com>
2025-09-10 15:00:15 +0800

    添加文档注释

--- apps/server/src/core/always_equal_proxy2.rs
+++ apps/server/src/core/always_equal_proxy2.rs
@@ -1,3 +1,16 @@
+//! 任意类型代理
+//!
+//! 将以下代码用rust实现
+//! ```python
+//! class AlwaysEqualProxy(str):
+//!         def __eq__(self, _):
+//!             return True
+//!         def __ne__(self, _):
+//!             return False
+//! ```
+//!
+//! 注意: 在python端, 使用json.dumps()会报错
+//!
 use pyo3::{prelude::*, types::PyString};
 
 #[pyclass(name = "AlwaysEqualProxy2")]
