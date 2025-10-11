e8180c0b80df9ac7526f910582ccf1ddd49dd04c
silent-rain <2367221387@qq.com>
2025-10-11 23:17:12 +0800

    取消sdxl

--- apps/server/src/model/mod.rs
+++ apps/server/src/model/mod.rs
@@ -1,9 +1,6 @@
 //! 模型
 
-use pyo3::{
-    Bound, PyResult, Python,
-    types::{PyModule, PyModuleMethods as _},
-};
+use pyo3::{Bound, PyResult, Python, types::PyModule};
 
 use crate::core::node::NodeRegister;
 
@@ -13,16 +10,18 @@ pub use nunchaku_sdxl_unet_loader::NunchakuSdxlUnetLoader;
 /// 逻辑模块
 pub fn submodule(py: Python<'_>) -> PyResult<Bound<'_, PyModule>> {
     let submodule = PyModule::new(py, "model")?;
-    submodule.add_class::<NunchakuSdxlUnetLoader>()?;
+    // submodule.add_class::<NunchakuSdxlUnetLoader>()?;
     Ok(submodule)
 }
 
 /// model node register
-pub fn node_register(py: Python<'_>) -> PyResult<Vec<NodeRegister<'_>>> {
-    let nodes: Vec<NodeRegister> = vec![NodeRegister(
-        "NunchakuSdxlUnetLoader",
-        py.get_type::<NunchakuSdxlUnetLoader>(),
-        "Nunchaku SDXL UNet Loader",
-    )];
+pub fn node_register(_py: Python<'_>) -> PyResult<Vec<NodeRegister<'_>>> {
+    let nodes: Vec<NodeRegister> = vec![
+        // NodeRegister(
+        //     "NunchakuSdxlUnetLoader",
+        //     py.get_type::<NunchakuSdxlUnetLoader>(),
+        //     "Nunchaku SDXL UNet Loader",
+        // ),
+    ];
     Ok(nodes)
 }
