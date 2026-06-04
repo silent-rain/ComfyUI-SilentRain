404dee012ca1223b47b4c6d272cd61b4db4c8cce
silent-rain <2367221387@qq.com>
2026-06-04 20:32:53 +0800

    修复编译问题

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -1,7 +1,1 @@
 from .comfyui_silentrain import *
-
-try:
-    from comfyui_silentrain_v3 import comfy_entrypoint
-    _v3_ext = comfy_entrypoint()
-except ImportError:
-    _v3_ext = None
--- scripts/build.sh
+++ scripts/build.sh
@@ -10,7 +10,6 @@ echo "====================== root:${root} ======================"
 
 # 项目名称, 一般不需要调整
 App=comfyui_silentrain
-AppV3=comfyui_silentrain_v3
 # 编译目录
 buildDir=${root}/target/wheels
 # 节点目录
@@ -44,40 +43,27 @@ source .venv/bin/activate
 # GUP 编译参数
 export NVCC_FLAGS="-D__CORRECT_ISO_CPP_MATH_H_PROTO"
 
-# build v1
-echo "build release whl for v1 ..."
-# uv tool run maturin build -r --auditwheel skip
+# build
+echo "build release whl ..."
 uv tool run maturin build -r
 
-# build v3
-echo "build release whl for v3 ..."
-cd ../server_v3
-uv tool run maturin build -r
-
-# 回到 nodes 处理
-cd ${root}
 
 # del old dir
 if [ -d ${nodesDir}/${App}.libs ]; then 
     rm -rf ${nodesDir}/${App}.libs/*
-    rm -rf ${nodesDir}/${AppV3}.libs/*
 fi
 
 
 # 获取whl文件路径
-echo "find whl files ..."
+echo "find whl file ..."
 whl=$(find ${buildDir} -name "${App}-*.whl" | head -n 1)
-echo "v1 whl file: ${whl}"
+echo "whl file: ${whl}"
 
-whl_v3=$(find ${buildDir} -name "${AppV3}-*.whl" | head -n 1)
-echo "v3 whl file: ${whl_v3}"
-
-# 覆盖解压 whl 包到 nodes 目录
+# 覆盖解压whl包
 echo "extract whl file ..."
 unzip -o ${whl} -d ${nodesDir}
-unzip -o ${whl_v3} -d ${nodesDir}
-
 echo -e "\n"
+
 
 tree -sh ${nodesDir}
 
